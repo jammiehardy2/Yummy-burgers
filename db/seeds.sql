ffc182d1851ea14456ea1264ffb3ee3d34ff1fb1
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-04-27 11:19:14 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+USE Burger_db
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Double Animal Style CheeseBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Chargrilled CheeseBurger', true);   
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Krusty Crabby Patty Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The ShackBurger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Castle Slider', true);   
+INSERT INTO burgers (burger_name, devoured) VALUES ('CheeseBurger', true); 
+
+
+SELECT *
+FROM burgers;
