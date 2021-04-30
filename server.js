6dd501ef46f7bfff15dc0c992f7dd92c172e0b06
Jammie <73665246+jammiehardy2@users.noreply.github.com>
2021-04-30 12:56:39 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var orm = require("./config/orm.js");
 var express = require("express");
+var exphbs = require("express-handlebars");
+const routes = require("./controllers/burgerController");
+
 var PORT = process.env.PORT || 8080;
+
 var app = express();
 
 app.use(express.static("public"));
@@ -8,12 +11,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
+
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/connection.js");
 app.use(routes);
+
 app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("listening on " + PORT);
 });
